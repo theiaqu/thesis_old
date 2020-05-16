4de4e77c80db698dbb208c0ff8cdd973623d0139
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-15 20:52:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
-let img;
+let img1;
 let img2;
-let d;
+let img3;
 function preload() {
-  img = loadImage('picture.jpg');
-  //img.mouseOver(changeGray);
+  img1 = loadImage('picture.jpg');
   img2 = loadImage('picture2.jpg');
-  //img2.mouseOver(changeGray);
-  d = 10;
+  img3 = loadImage('texture.jpg')
 }
 
-function setup() {
-  createCanvas(1000,1000);
-  image(img, 0, 0);
-  tint(0, 153, 204); // Tint blue
-  image(img2, 500, 500);
-}
+// function setup() {
+//   createCanvas(1000,1000);
+//   image(img, 0, 0);
+//   tint(0, 153, 204); // Tint blue
+//   image(img2, 500, 500);
+// }
 
-function changeGray() {
-  d = d + 10;
-  if (d > 100) {
-    d = 0;
-  }
+function draw() {
+  x = map(mouseY, 0, 500, 0, -250);
+  w = map(mouseY, 0, 500, 600, 1000);
+
+  a = map(mouseX, 0, 670, -151, 0);
+
+
+
+  background(220);
+  image(img3, a, 0);
+  image(img1, a, -21);
+	image(img2, x+a, 0, w, w);
+
 }
