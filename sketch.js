2463d533ecdd207025ec9eb084d66b45c917b3c2
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-15 20:20:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let img;
+let img2;
 function preload() {
   img = loadImage('picture.jpg');
+  img2 = loadImage('picture2.jpg');
 }
 function setup() {
-  createCanvas(2000,2000)
+  createCanvas(1000,1000)
   image(img, 0, 0);
+  image(img2, 100, 100);
 }
