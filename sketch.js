597d3c4ce5312efef2de485b56b11d9f8e953997
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-17 13:13:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,19 @@ let img;
 let img2;
 function preload() {
   img = loadImage('picture.jpg');
-  img2 = loadImage('picture2.jpg');
-  img2.resize(50, 100)
-}
+
+  // Image warps because not scaling proportionally
+  imageMode(CORNER);
+  image(img2, 0, 0, width, height);
+
+  // Image scales proportionally
+  imageMode(CORNER);
+  image(img2, 0, 0, width, img.height*width/img.width); // to fit width
+
+  // Placing image on screen and keeping in proportion
+  var scale = 0.8;
+  imageMode(CENTER);
+  image(img2, 0.5*width, 0.5*height, scale*width, scale*img.height*width/img.width); // to fit width}
 
 function setup() {
   createCanvas(1000,1000);
