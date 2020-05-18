87c984d63df514ccaa21c10063e710b85f5d5f79
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-17 20:57:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ let img2;
 function preload() {
   img = loadImage('picture.jpg');
   img2 = loadImage('picture2.jpg');
-  img2.resize(100, 100)
 }
 
 function setup() {
   createCanvas(1000,1000);
   image(img, 0, 0);
   tint(0, 153, 204); // Tint blue
+  img2.resize(150, 0);
   image(img2, 500, 500);
 }
