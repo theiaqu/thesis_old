5983c8efe0f39542e4a6cedbec4ec0cd4afb1f4b
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-18 23:07:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,35 @@
+// cd Sites/thesis
+// php -S localhost:8000
+
 let img;
 let img2;
+var color = 0;
+var angle = 0;
+
 function preload() {
   img = loadImage('picture.jpg');
   img2 = loadImage('picture2.jpg');
 }
 
 function setup() {
-  background(255);
+  background(0);
   createCanvas(1000,1000);
-}
-
-function draw() {
   image(img, 0, 0);
   tint(0, 153, 204); // Tint blue
   img2.resize(150, 0);
   image(img2, 500, 500);
-  ellipse(mouseX, mouseY, 20, 20)
+  textFont("Arial");
+  textSize(22);
+}
+
+function draw() {
+  push();
+  translate(mouseX, mouseY);
+  rotate(radians(angle));
+  line(-50, -50, 50, 50);
+  line(50, -50, -50, 50);
+  angle++;
+  pop();
+  noFill();
+  rect(10, 10, width-20, height-20);
 }
