1048c54594c9239f205297f82dc12322e6a2dbc3
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-05-19 14:39:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,20 +10,20 @@ var bug;
 function preload() {
   img1 = loadImage('picture.jpg');
   img2 = loadImage('picture2.jpg');
-  texture = loadImage('texture.jpg'); 
+  texture = loadImage('texture.jpg');
 }
 
 function setup() {
-  background(0);
-  createCanvas(1000,1000);
-  image(img1, random(width - 100), random(height - 250));
+  background(220);
+  createCanvas(800,800);
+  image(img1, random(width - 250), random(height - 250));
   img2.resize(150, 0);
   filter(GRAY);
-  image(img2, random(width - 100), random(height - 250));
-  blend(texture, 0, 0, 900, 600, 0, 0, 900, 600, SOFT_LIGHT);
+  image(img2, random(width - 250), random(height - 250));
   // filter(INVERT);
   textFont("Arial");
   textSize(22);
+  blend(texture, 0, 0, 500, 500, 0, 0, width, height, SOFT_LIGHT);
 }
 
 function draw() {
